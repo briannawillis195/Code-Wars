c06100cc449098d09eb0be1b85677f30a26a15cd
brianna willis <brianna.willis195@gmail.com>
2023-10-13 23:22:48 -0400

    daily practice

---
+++ Num_to_String.js
@@ -0,0 +1,8 @@
+//We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    // Return a string of the number here!
+    let n = num
+    let text = num.toString()
+    return text
+  }
