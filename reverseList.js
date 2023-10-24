3d2fdd130ff29b5b1c5b636baa53b0e21bc7e2dc
brianna willis <brianna.willis195@gmail.com>
2023-10-24 10:00:01 -0400

    daily commit

---
+++ reverseList.js
@@ -0,0 +1,8 @@
+//In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+function reverseList(list) {
+
+    let reversed = list.reverse()
+    return reversed
+    
+    }
