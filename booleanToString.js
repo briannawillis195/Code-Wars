1a80caf299863bafdc0657a50adb2080c7839078
brianna willis <brianna.willis195@gmail.com>
2023-11-10 12:54:28 -0500

    daily commit

---
+++ booleanToString.js
@@ -0,0 +1,10 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+  //your code here
+  if(b === true){
+    return b.toString()
+  }else if (b === false){
+    return b.toString()
+  }
+}
